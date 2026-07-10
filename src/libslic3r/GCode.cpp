ed703a56e62d298a98b6e0e6a7509217d5f8dd3c
BuildTools <imnotednamode@gmail.com>
2026-07-10 11:17:43 -0600

    fix monotonic role assigment and remove wipe

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -7723,8 +7723,8 @@ std::string GCode::retract(bool toolchange, bool is_last_retraction, LiftType li
     if (m_writer.filament() == nullptr)
         return gcode;
 
-    // wipe (if it's enabled for this extruder and we have a stored wipe path and no-zero wipe distance)
-    if (FILAMENT_CONFIG(wipe) && m_wipe.has_path() && scale_(FILAMENT_CONFIG(wipe_distance)) > SCALED_EPSILON) {
+    // wipe (if it's enabled for this extruder and we have a stored wipe path and no-zero wipe distance and not wave overhang)
+    if (FILAMENT_CONFIG(wipe) && m_wipe.has_path() && scale_(FILAMENT_CONFIG(wipe_distance)) > SCALED_EPSILON && role != erWaveBridgeInfill) {
         Wipe::RetractionValues wipeRetractions = m_wipe.calculateWipeRetractionLengths(*this, toolchange);
         gcode += toolchange ? m_writer.retract_for_toolchange(true,wipeRetractions.retractLengthBeforeWipe) : m_writer.retract(true, wipeRetractions.retractLengthBeforeWipe);
         gcode += m_wipe.wipe(*this,wipeRetractions.retractLengthDuringWipe, toolchange, is_last_retraction);
